aca47f985a1c2cf9012075d6357a5b7142dcbe68
Zak Fargo <zak@fargobose.com>
2020-12-24 13:09:12 -0800

    fixing build file - project location

---
+++ src/Transcode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RaccoonWars.Transcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
