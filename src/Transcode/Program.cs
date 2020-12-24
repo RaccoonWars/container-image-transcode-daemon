aac59cf28a4750dea42b92204ed7ac4b1918d3ce
Zak Fargo <zak@fargobose.com>
2020-12-24 13:35:36 -0800

    fixing dockerfile - trying different copying mechanism

--- src/Transcode/Program.cs
+++ src/Transcode/Program.cs
@@ -1,11 +1,20 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace RaccoonWars.Transcode
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            string configurationFile = Environment.GetEnvironmentVariable("TRANSCODE_CONFIGURATION_PATH");
+
+            if(string.IsNullOrWhiteSpace(configurationFile))
+            {
+
+            }
+
+
             Console.WriteLine("Hello World!");
         }
     }
